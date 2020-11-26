a7ab2a4de64f547f502aa2fa80ca004cfc99c30f
mholwill <m_holwill@hotmail.co.uk>
2020-11-26 13:44:17 +0000

    Tested Knight attack and defend and added Barbarian

--- src/main/java/characters/Dwarf.java
+++ src/main/java/characters/Dwarf.java
@@ -30,4 +30,6 @@ public class Dwarf extends Fighter{
 
 
 
+
+
 }
---
+++ src/main/java/characters/Knight.java
@@ -0,0 +1,25 @@
+package characters;
+
+import weapons.Weapon;
+
+public class Knight extends Fighter {
+
+    private int armour;
+
+    public Knight(int maxHealth, String name, Weapon activeWeapon, int armour) {
+        super(maxHealth, name, activeWeapon);
+        this.armour = armour;
+    }
+
+    public int getArmour() {
+        return armour;
+    }
+
+    public void setArmour(int armour) {
+        this.armour = armour;
+    }
+
+    public void defend(int damage) {
+        super.defend(damage - armour);
+    }
+}
